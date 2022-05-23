e72e8d0d7ca328c5913fb426ee4f688ee9e4eba8
Andreas Peters <ap@aventer.biz>
2022-05-23 17:36:33 +0200

    ADD: nix shell file.

---
+++ shell.nix
@@ -0,0 +1,20 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+name = "python-env";
+
+buildInputs = [
+  vagrant
+];
+
+SOURCE_DATE_EPOCH = 315532800;
+PROJDIR = "${toString ./.}";
+
+shellHook = ''
+  cd vagrant
+  vagrant plugin repair
+  vagrant up --provider=libvirt
+  '';
+}  
+
+
